f2fe3e6f19770d681da61d81f877bdc68c5c0151
Finding_nemo <kamalsingh1472004@gmail.com>
2025-10-03 00:41:59 +0530

    user.controller.js

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -1,6 +1,6 @@
 import asyncHandler from "express-async-handler"
 import User from "../models/user.model.js"
-import { getAuth } from "@clerk/express";
+import { clerkClient, EmailAddress, getAuth } from "@clerk/express";
 
 export const getUserProfile = asyncHandler(async (req , res) =>{
     const {username} = req.params;
@@ -23,6 +23,27 @@ export const updateProfile = asyncHandler(async (req, res)=>{
 
 
 export const syncUser = asyncHandler(async (req, res)=>{
+    const {userId} = getAuth(req);
 
+    //check if user already exists in mongoDB
+    const existingUser = await User.findOne({clerkId:userId});
+    if(existingUser){
+        return res.status(200).json({user:existingUser,message:"User already exists"});
+    }
 
+    // create new user from Clerk data
+    const clerkUser = await clerkClient.users.getUser(userId);
+
+    const userData = {
+        clerkId: userId,
+        email: clerkUser.emailAddresses[0].emailAddress,
+        firstName: clerkUser.firstName || "",
+        lastName: clerkUser.lastName || "",
+        username: clerkUser.emailAddresses[0].emailAddress.split("@")[0],
+        profilePicture: clerkUser.imageUrl || "",
+    };
+
+    const user = await User.create(userData);
+
+    res.status(201).json({user,message:"User created successfully"});
 });
--- Backend/src/routes/user.route.js
+++ Backend/src/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { getUserProfile } from "../controllers/user.controller.js";
+import { getUserProfile, syncUser } from "../controllers/user.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
